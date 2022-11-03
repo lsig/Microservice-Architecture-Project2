84caed8339c6fd09dea28e2ff1d99fcb7cf2d451
Lsig <logis21@ru.is>
2022-11-03 14:20:28 +0000

    event sending works, still a database problem

--- PaymentService/main.py
+++ PaymentService/main.py
@@ -14,13 +14,13 @@ def create_app(container: Container) -> FastAPI:
 settings = Settings("./infrastructure/.env")
 container = Container()
 container.config.from_pydantic(settings)
-#container.wire(modules=[endpoints])
+container.wire(modules=[endpoints])
 
-#app = create_app(container)
+app = create_app(container)
 receiver = container.order_receiver_provider()
 
 
 if __name__ == '__main__':
-    #uvicorn.run('main:app', host='0.0.0.0', port=8004, reload=True)
+    uvicorn.run('main:app', host='0.0.0.0', port=8004, reload=True)
     print("Waiting for ...")
     receiver.consume()
