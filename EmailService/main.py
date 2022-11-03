d0b682210fbf4750315bdba0476bff81f0835f80
Lsig <logis21@ru.is>
2022-11-03 15:02:26 +0000

    Threading in place, not recommended for pika use celery but for the purpose of the program it works

--- EmailService/main.py
+++ EmailService/main.py
@@ -1,5 +1,6 @@
 from infrastructure.container import Container
 from infrastructure.settings import Settings
+import threading
 
 settings = Settings("./infrastructure/.env")
 container = Container()
@@ -8,8 +9,9 @@ container.config.from_pydantic(settings)
 order_receiver = container.order_receiver_provider()
 payment_receiver = container.payment_receiver_provider()
 
+start_consumer = threading.Thread(target=order_receiver.consume)
+start_reciever = threading.Thread(target=payment_receiver.consume)
 
 if __name__ == '__main__':
-    print("Waiting for ...")
-    order_receiver.consume()
-    payment_receiver.consume()
+    start_consumer.start()
+    start_reciever.start()
